997aec436d822cdfb5c0297df613d5952570e41b
apoznar <adampoznar@gmail.com>
2017-02-06 18:27:34 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='kpstat',
+      version='1.0',
+      description='Python Statistics',
+      author='Damian Kacperski, Adam Poznar',
+      author_email='adampoznar@gmail.com',
+      download_url='https://github.com/kacperskid/bioinfo-statystyka',
+      packages=['scipy', 'pandas', 'collections'],
+     )
